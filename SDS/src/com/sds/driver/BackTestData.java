4a8167d5611c1c594debdbb5166a9dbcb715304d
joma <joma@joma-VirtualBox>
2020-05-25 21:08:04 -0500

    add pink process

--- SDS/src/com/sds/driver/BackTestData.java
+++ SDS/src/com/sds/driver/BackTestData.java
@@ -10,11 +10,17 @@ public class BackTestData {
 		// TODO Auto-generated method stub
 		long t1 = System.currentTimeMillis();
 		String path = "/home/joma/share/test/simple/";
-		String symbol = "TSLA";
+		String symbol = "BAC";
 		BackTestBaseCVS.processStock(path, symbol);
+		System.out.println("Processing teal records...");
 		BackTestTealCVS.processStock(path, symbol);
+		System.out.println("Processing yellow records...");
 		BackTestYellowCVS.processStock(path, symbol);
+		System.out.println("Processing pink records...");
+		BackTestPinkCVS.processStock(path, symbol);
+		System.out.println("Processing summary...");
 		Summary.processStock(symbol);
+		System.out.println("Processing PT9...");
 		PT9.processStock(symbol);
 		long t2 = System.currentTimeMillis();
 		System.out.println("Time cost is "+((t2-t1)*1.0f)/(1000*60.0f)+" minutes");
