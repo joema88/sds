a07d02e219357d1718c1b72a3079d819c0738fb1
joma <joma@joma-VirtualBox>
2020-05-31 22:11:32 -0500

    Bull Pattern Two

--- SDS/src/com/sds/analysis/Summary.java
+++ SDS/src/com/sds/analysis/Summary.java
@@ -3,6 +3,7 @@ package com.sds.analysis;
 import java.sql.*;
 
 import com.sds.db.DB;
+import com.sds.db.TwoBullDB;
 
 public class Summary {
 
@@ -11,7 +12,9 @@ public class Summary {
 	private static PreparedStatement scUpdateStmnt = null;
 	private static PreparedStatement yp10SumCalStmnt = null;
 	private static PreparedStatement yp10SumUpdateStmnt = null;
-
+	private static PreparedStatement queryCX520 = null;
+	private static PreparedStatement findPreviousCCX = null;
+	private static PreparedStatement updateCCX = null;
 
 	public static void init() {
 		queryStmnt = DB.getSymbolDateIDQueryStmnt();
@@ -19,12 +22,54 @@ public class Summary {
 		scUpdateStmnt = DB.getSCUpdateStmnt();
 		yp10SumCalStmnt = DB.getYP10SumCalStmnt();
 		yp10SumUpdateStmnt = DB.getYP10SumUpdateStmnt();
+		queryCX520 = TwoBullDB.getCurrentCX520Stmnt();
+		findPreviousCCX = TwoBullDB.getPreviousCCXStmnt();
+		updateCCX = TwoBullDB.getCCXUpdateStmnt();
+
 	}
 
 	public static void processDailyStocks(String date) {
 		init();
 		int dateID = DB.getDateID(date);
 
+	}
+	
+	public static void processCCXHistory(String symbol, int stockID) {
+		init();
+		if (symbol != null && symbol.length() > 0) {
+			stockID = DB.getSymbolID(symbol);
+		}
+		try {
+			queryCX520.setInt(1, stockID);
+			ResultSet rs4 = queryCX520.executeQuery();
+			while(rs4.next()) {
+				int cx520 = rs4.getInt(1);
+				int dateID = rs4.getInt(2);
+				findPreviousCCX.setInt(1, stockID);
+				findPreviousCCX.setInt(2, dateID);
+				ResultSet rs5 = findPreviousCCX.executeQuery();
+				if (rs5.next()) {
+					int pccx = rs5.getInt(1);
+					int ccx = 0;
+					if((pccx>0 && cx520>0)||(pccx<0 && cx520<0)) {
+						ccx = pccx + cx520;
+					}else if((pccx>0 && cx520<0)||(pccx<0 && cx520>0)) {
+						ccx = cx520;
+					}else if(pccx == 0) {
+						ccx = cx520;
+					}
+					
+					updateCCX.setInt(1, ccx);
+					updateCCX.setInt(2, stockID);
+					updateCCX.setInt(3, dateID);
+					updateCCX.executeUpdate();
+
+				}
+
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace(System.out);
+		}
 	}
 
 	public static void processStock(String symbol, int stockID) {
@@ -54,7 +99,7 @@ public class Summary {
 					scUpdateStmnt.executeUpdate();
 
 				}
-				
+
 				yp10SumCalStmnt.setInt(1, stockID);
 				yp10SumCalStmnt.setInt(2, dateID - 9);
 				yp10SumCalStmnt.setInt(3, dateID);
@@ -62,13 +107,16 @@ public class Summary {
 				if (rs3.next()) {
 					int yellowSum = rs3.getInt(1);
 					int pinkSum = rs3.getInt(2);
-					int YP10 = yellowSum + 2*pinkSum ;
+					int YP10 = yellowSum + 2 * pinkSum;
 					yp10SumUpdateStmnt.setInt(1, YP10);
 					yp10SumUpdateStmnt.setInt(2, stockID);
 					yp10SumUpdateStmnt.setInt(3, dateID);
 					yp10SumUpdateStmnt.executeUpdate();
 
 				}
+
+			
+
 			}
 
 		} catch (Exception ex) {
---
+++ SDS/src/com/sds/db/TwoBullDB.java
@@ -0,0 +1,86 @@
+package com.sds.db;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*
+ * Observation: if after teal9 +5% continued for 10days then drop into bear or correction
+   then wait AVG5>AVG20 for 10 continuous days, before buy. This could be W recover 
+   or V recover, both applicable. TNDM, WST, MSFT. The uptrend should not have AVG5<AVG20
+   to make this rule more accurate.
+   Alternatively: 30 day plus AVG5>AVG20 with 1 Teal9, drop, then AVG5>AVG20 for days buy
+   BIG
+ */
+public class TwoBullDB extends DB {
+	private static PreparedStatement findPreviousCCXStmnt = null;
+	private static PreparedStatement queryCX520 = null;
+	private static PreparedStatement updateCCXStmnt = null;
+
+	public static void closeConnection() {
+		try {
+			if(updateCCXStmnt != null) {
+				updateCCXStmnt.close();
+				updateCCXStmnt = null;
+			}
+			if(findPreviousCCXStmnt != null) {
+				findPreviousCCXStmnt.close();
+				findPreviousCCXStmnt = null;
+			}
+			if(queryCX520 != null) {
+				queryCX520.close();
+				queryCX520 = null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static PreparedStatement getCCXUpdateStmnt() {
+	   if(updateCCXStmnt == null) {
+		   try {
+
+				String query = "UPDATE BBROCK SET CCX = ? WHERE  STOCKID = ? AND DATEID = ? ";
+
+				updateCCXStmnt = DB.getConnection().prepareStatement(query);
+			} catch (SQLException e) {
+				e.printStackTrace(System.out);
+			}
+	   }
+	   return updateCCXStmnt;
+	}
+	
+	public static PreparedStatement getCurrentCX520Stmnt() {
+
+		if (queryCX520 == null) {
+			try {
+
+				String query = "SELECT CX520, DATEID FROM BBROCK WHERE  STOCKID = ? ORDER BY DATEID ASC ";
+
+				queryCX520 = DB.getConnection().prepareStatement(query);
+			} catch (SQLException e) {
+				e.printStackTrace(System.out);
+			}
+		}
+
+		return queryCX520;
+	}
+
+	
+	public static PreparedStatement getPreviousCCXStmnt() {
+
+		if (findPreviousCCXStmnt == null) {
+			try {
+
+				String query = "SELECT CCX FROM BBROCK WHERE  STOCKID = ? AND DATEID <? ORDER BY DATEID DESC LIMIT 1";
+
+				findPreviousCCXStmnt = DB.getConnection().prepareStatement(query);
+			} catch (SQLException e) {
+				e.printStackTrace(System.out);
+			}
+		}
+
+		return findPreviousCCXStmnt;
+	}
+
+}
