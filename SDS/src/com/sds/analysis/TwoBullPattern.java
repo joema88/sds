643f4e374f93e026d4998aa70200a4ef84b110f3
joma <joma@joma-VirtualBox>
2020-06-02 22:49:39 -0500

    BULL PATERN TWO LOGIC THINKING/DESIGN

---
+++ SDS/src/com/sds/analysis/TwoBullPattern.java
@@ -0,0 +1,34 @@
+package com.sds.analysis;
+
+/*
+ *  So Bull Pattern deals is another less bull case of Bull Pattern One.
+ *  Strong Bull Pattern One, the BT9 and 10 pass points usually overlapped and one after 
+ *  another without much break period. Most stocks could not behave this strongly.
+ *  What usually happens is that after T9, with or without 10 passing points  or sometimes
+ *  even with a couple around 10 passing points, the stock enters into correction without
+ *  another leg up T9. The correct usually last above 25 days and above 15% drop, during 
+ *  correction period, AVG5 < AVG 20 (>=25 days), then comes with AVG5>AVG20 time.
+ *  If it last longer than >12 days, then an relative safe up trend is set up. 
+ *  The above 12 days AVG5>AVG20 may take one short above or several such before appears
+ *  Since most stocks fall into this category, this may be a more important case than 
+ *  bull pattern one. Bull pattern one usually find exceptional bull stocks, especially
+ *  BT9 points are back to back closely.
+ *  Pattern Two is usually good for pick up relative good stock on cheap and wait for up leg.
+ *  Example: BIG, GNUS, TNDM,
+ *  IF there is BT9 appears, then we could buy right after BT9 without +5% increase
+ *  especially the SUM (TEAL+PINK+Yellow) for the BT9 period is >=5, even part of the BT9
+ *  days is AVG5 <AVG20 period. That way you get much cheap entry buy point. (see FBIO)
+ */
+/*
+ * ONE TRICK IS TO MERGE SMALL FRAGMENTS (LESS THAN 13 DAYS) OF AVG5<AVG20 OR AVG5>AVG20 
+ * INTO BIG CHUNK OF TREND, SMALL FRAGMENT AVG5>AVG20 PEAK (HIGHEST CLOSE) COULD BE USED
+ * AS REFERENCE TO SEE IF THE FRAGMENT SHOULD BE ALIGNED BACKWARDS OR FORWARDS, UP OR DOWN
+ */
+public class TwoBullPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
